dcd2ce2f7605a9c920c116c27f9571e2feaa181d
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-06-19 17:14:59 -0300

    Uptade Feed

--- src/pages/Feed/feed.js
+++ src/pages/Feed/feed.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import HeaderMain from '../../components/HeaderMain/HeaderMain'
+import More from '../../images/more.svg'
+import './feed.css'
 
 function Feed() {
   return (
     <div>
       <HeaderMain />
+
+      <main>
+        <div className="cards">
+          <div className="card">
+            <header>
+              <h2>Texto 01</h2>
+              <img src={More} />
+            </header>
+            <div className="line"></div>
+            <p>textotextotexto</p>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
