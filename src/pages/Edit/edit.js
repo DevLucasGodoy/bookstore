f4db1c514ba6cd4e515c42ce14899e6564710f67
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-06-19 17:03:30 -0300

    Update File

--- src/pages/Edit/edit.js
+++ src/pages/Edit/edit.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 function Edit() {
   return (
     <div>
+      <Header />
       <h1>Page Edit</h1>
     </div>
   )
